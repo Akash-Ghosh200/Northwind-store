d8be284771b98c544d5f3662b0ca6d6b81dd8d0e
Akash Ghosh <ag9749380@gmail.com>
2026-05-12 20:49:34 +0530

    feat: implement product catalog and home page components; add image optimization utility and seed script

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Show, SignInButton, SignUpButton, useAuth, UserButton } from '@clerk/react'
 import PageLoader from './components/PageLoader';
 import Layout from './components/Layout';
+import { Route, Routes } from 'react-router';
+import HomePage from './pages/HomePage';
 
 
 function App() {
@@ -10,20 +12,9 @@ function App() {
 
    return (
     <Layout>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-
-      <p className='text-red-600  font-extrabold text-4xl'>Hello</p>
-      <button className='btn btn-primary'>Click Me</button>
-      <button className='btn btn-secondary'>Click Me</button>
-      <button className='btn btn-outline'>Click Me</button>
+       <Routes>
+        <Route path='/' element={<HomePage />} />
+       </Routes>
     </Layout>
   )
 }
